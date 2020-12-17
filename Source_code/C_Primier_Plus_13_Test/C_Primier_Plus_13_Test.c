82465d26445f66c866a60ef084a55bb6d179df81
Michel.lu <15071076364@163.com>
2020-12-17 09:22:12 +0800

    Modiley C_Primier_Plus_13_Test.c

--- Source_code/C_Primier_Plus_13_Test/C_Primier_Plus_13_Test.c
+++ Source_code/C_Primier_Plus_13_Test/C_Primier_Plus_13_Test.c
@@ -10,7 +10,7 @@
 
 
 
-int main(int argc, char** argv)
+int main()
 {
 	//1.修改程序清单13.1中的程序，要求提示用户输入文件名，并读取用户输入的信息，不使用命令行参数。
     //int ch;
@@ -58,26 +58,36 @@ int main(int argc, char** argv)
     //该程序要使用ctype.h中的toupper()函数，在写入到输出文件时把所有文本转换成大写。使用标准I/O和文本模式。
     //定义一个文件流与一个字符串接收文件名和一个字符 → 提示用户输入文件名 → 输入文件名 → 只读模式打开文件
     //fgetc读取字符 → if判断是否为小写字母，小写转大写，写入临时字符串 → 关闭文件 → 只写模式打开文件 → 写入文件 → 关闭文件
-    FILE* fp;
-    char file_name[256];
-    char temp[LEN];
-    char ch;
-    printf("Please enter the file name which you want to open: ");
-    scanf("%s", file_name);
-    if ((fp = fopen(file_name, "r")) == NULL)
-    {
-        fprintf(stderr, "Can't open the %s file.", file_name);
-        exit(EXIT_FAILURE);
-    }
-    while ((ch = fgetc(fp)) != EOF)
-    {
-        if (ch >= 97 && ch <= 122)
-        {
-            ch = toupper(ch);
-        }
-
-    }
-
+    // FILE* fp;
+    // char file_name[256];
+    // char temp[LEN];
+    // int count = 0;
+    // char ch;
+    // printf("Please enter the file name which you want to open: ");
+    // scanf("%s", file_name);
+    // if ((fp = fopen(file_name, "r")) == NULL)
+    // {
+    //     fprintf(stderr, "Can't open the %s file.", file_name);
+    //     exit(EXIT_FAILURE);
+    // }
+    // while ((ch = fgetc(fp)) != EOF)
+    // {
+    //     if (ch >= 97 && ch <= 122)
+    //     {
+    //         ch = toupper(ch);
+    //     }
+    //     temp[count] = ch;
+    //     count++;
+    // }
+    // temp[count] = '\0';
+    // fclose(fp);
+    // if ((fp = fopen(file_name, "w")) == NULL)
+    // {
+    //     fprintf(stderr, "Can't open the %s file.", file_name);
+    //     exit(EXIT_FAILURE);
+    // }
+    // fputs(temp, fp);
+    // fclose(fp);
     
 
 
