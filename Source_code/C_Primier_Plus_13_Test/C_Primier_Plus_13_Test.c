8fc31eed5e4cfc42a8169ffe073b8a8114a0ef48
Michel.lu <15071076364@163.com>
2020-12-17 08:13:16 +0800

    Modifly C_Primier_Plus_13_Test.c

--- Source_code/C_Primier_Plus_13_Test/C_Primier_Plus_13_Test.c
+++ Source_code/C_Primier_Plus_13_Test/C_Primier_Plus_13_Test.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
+
 
 
 
@@ -52,8 +54,31 @@ int main(int argc, char** argv)
     fclose(input);*/
     
 
+    //3.编写一个文件拷贝程序，提示用户输入文本文件名，并以该文件名作为原始文件名和输出文件名。
+    //该程序要使用ctype.h中的toupper()函数，在写入到输出文件时把所有文本转换成大写。使用标准I/O和文本模式。
+    //定义一个文件流与一个字符串接收文件名和一个字符 → 提示用户输入文件名 → 输入文件名 → 只读模式打开文件
+    //fgetc读取字符 → if判断是否为小写字母，小写转大写，写入临时字符串 → 关闭文件 → 只写模式打开文件 → 写入文件 → 关闭文件
+    FILE* fp;
+    char file_name[256];
+    char temp[LEN];
+    char ch;
+    printf("Please enter the file name which you want to open: ");
+    scanf("%s", file_name);
+    if ((fp = fopen(file_name, "r")) == NULL)
+    {
+        fprintf(stderr, "Can't open the %s file.", file_name);
+        exit(EXIT_FAILURE);
+    }
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        if (ch >= 97 && ch <= 122)
+        {
+            ch = toupper(ch);
+        }
 
+    }
 
+    
 
 
 
