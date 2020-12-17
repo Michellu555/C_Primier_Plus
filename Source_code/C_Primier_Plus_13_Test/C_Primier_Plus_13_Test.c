bed29584eac82dcb58a1420dd614ee40933b7548
Michel.lu <15071076364@163.com>
2020-12-18 00:19:05 +0800

    Modifly C_Primier_Plus_13_Test.c

--- Source_code/C_Primier_Plus_13_Test/C_Primier_Plus_13_Test.c
+++ Source_code/C_Primier_Plus_13_Test/C_Primier_Plus_13_Test.c
@@ -259,6 +259,27 @@ int main(int argc, char** argv)
     */
 
 
+    //11.编写一个程序，接受两个命令行参数。第1个参数是一个字符串，第2个参数是一个文件名。
+    //然后该程序查找该文件，打印文件中包含该字符串的所有行。因为该任务是面向行而不是面向字符的，所以要使用fgets()而不是getc()。
+    //使用标准C库函数strstr()(11.5.7节简要介绍过）在每一行中查找指定字符串。假设文件中的所有行都不超过255个字符。
+    //定义文件流 → 打开指定文件 → fgets循环录入每一行 → 判断是否包含目标字符串 → 输出目标字符串所在的行
+    //FILE *fp;
+    //char temp[SLEN];
+    //if ((fp = fopen(argv[2], "r")) == NULL) //打开指定文件
+    //{
+    //    fprintf(stderr, "Can't open the %s file.\n", argv[2]);
+    //    exit(EXIT_FAILURE);
+    //}
+    //while ((fgets(temp, SLEN, fp)) != NULL) //fgets循环录入每一行
+    //{
+    //    if ((strstr(temp, argv[1])) != NULL) //判断是否包含目标字符串
+    //    {
+    //        fputs(temp, stdout); //输出目标字符串所在的行
+    //    }
+    //}
+    //fclose(fp);
+
+
 
 
 
