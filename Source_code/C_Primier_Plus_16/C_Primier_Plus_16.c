34753eea329abea8b2fcfa7087b963843c8c5a38
Michel.lu <15071076364@163.com>
2021-01-08 08:13:07 +0800

    Modifly C_Primier_Plus_16.c

--- Source_code/C_Primier_Plus_16/C_Primier_Plus_16.c
+++ Source_code/C_Primier_Plus_16/C_Primier_Plus_16.c
@@ -7,6 +7,8 @@ tive.-Oscar Wilde"
 #define FOUR TWO * TWO
 #define PX printf("X is %d.\n", x);
 #define FMT "X is %d.\n"
+#define SQUARE(X) X*X
+#define PR(X) printf("The reault is %d.\n",X)
 
 
 int main()
@@ -19,6 +21,30 @@ int main()
 	//printf(FMT, x);
 	//printf("%s\n", OW);
 	//printf("TWO:OW\n");
+
+
+	//程序清单16.2
+	//mac_arg.c -- 带参数的宏
+	int x = 5;
+	int z;
+	printf("x = %d\n", x);
+	z = SQUARE(x); //z = x * x
+	printf("Evaluation SQUARE(x):");
+	PR(z);
+	//z = SQUARE(2); // z = 2 * 2
+	//printf("Evaluation SQUARE(2):");
+	//PR(z);
+	printf("Evaluation SQUARE(x+2):");
+	PR(SQUARE(x + 2)); //(x + 2)* (x + 2) → （5 + 2） * （5 + 2）
+	printf("Evaluation 100/SQUARE(2):");
+	PR(100 / SQUARE(2));
+	printf("x is %d.\n", x);
+	printf("Evaluation SUQARE(++x):");
+	PR(SQUARE(++x));
+	printf("After incrementiong, x is %x.\n", x);
+
+
+
 
 
 
